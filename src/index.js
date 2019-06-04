12a9318f3eb346b29d263a18c0d9eb2be73df5a5
Miguel <miaortizma@unal.edu.co>
2019-06-03 19:20:55 -0500

    late commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Color from "./Color";
+import Clock from './Clock'
+import Request from './Request'
+
+class App extends Component{
+	render(){
+		return(
+      <Fragment>
+			 <Color />
+       <Clock /> 
+       <Request />
+			</Fragment>
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
